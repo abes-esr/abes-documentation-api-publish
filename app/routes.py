7569e9937f30e204054f29f0d5d937b503455d99
Clément Delafontaine <clment.delafontaine@gmail.com>
2025-04-17 12:40:39 +0200

    feat: adding tag names for better organisation

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 import os
 from enum import Enum
-
+import logging
 from fastapi import APIRouter, HTTPException, Query
 from pydantic import BaseModel
-
 from .services.deployment_service import deploy_manuals, list_manuals, deploy_all_manuals,  \
     load_json_config, config_directory, purge_directory_list
-import logging
 
 router = APIRouter()
 
@@ -17,42 +15,42 @@ class Manual(BaseModel):
     name: str
     name: ManualEnum
 
-@router.get("/liste")
-async def list_available_manuals():
+@router.get("/liste", tags=["Lister"])
+async def lister_les_manuels_disponibles_dans_l_API():
     """
     Donne la liste de tous les manuels de la base de données de l'API
     """
     return list_manuals()
 
 
-@router.put("/deploy")
-async def deploy(manuals: list[ManualEnum] = Query(...)):
+@router.put("/deploy", tags=["Déployer"])
+async def deployer_un_ou_plusieurs_manuels(manuals: list[ManualEnum] = Query(...)):
     """
     Déploie un ou plusieurs manuels en purgeant les fichiers scenari.
     """
     status = deploy_manuals(manuals)
     if not status["success"]:
-        raise HTTPException(status_code=500, detail="Deployment failed")
+        raise HTTPException(status_code=500, detail="Erreur de déploiement")
     return status
 
-@router.put("/deploy_all")
-async def deploy_all():
+@router.put("/deploy_all", tags=["Déployer"])
+async def deployer_tous_les_manuels():
     """
     Déploie tous les manuels en purgeant les fichiers scenari.
     """
     status = deploy_all_manuals()
     if not status["success"]:
-        raise HTTPException(status_code=500, detail="Deployment failed")
+        raise HTTPException(status_code=500, detail="Erreur de déploiement")
     return status
 
-@router.delete("/purge")
-async def purge_directory(manuals: list[ManualEnum] = Query(...)):
+@router.delete("/purge", tags=["Purger"])
+async def purger_un_ou_plusieurs_manuels(manuals: list[ManualEnum] = Query(...)):
     """
     Purge les fichiers scenari des manuels en entrée. Liste des dossiers et fichiers scenari : skin, res, co, lib-md, meta, lib-sc, index.html
     """
     status = purge_directory_list(manuals)
     if not status["success"]:
-        raise HTTPException(status_code=500, detail="Purge failed")
+        raise HTTPException(status_code=500, detail="Erreur lors de la suppression du manuel")
     return status
 
 def init_routes(app):
